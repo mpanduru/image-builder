c542935165cf5011c66dc5a315d39e1bc5148a8a
Matt Boersma <Matt.Boersma@microsoft.com>
2023-06-14 13:55:47 -0600

    Add support for Azure Linux (CBL-Mariner 2)

--- images/capi/azure_targets.sh
+++ images/capi/azure_targets.sh
@@ -1,5 +1,5 @@
-VHD_TARGETS="ubuntu-1804 ubuntu-2004 ubuntu-2204 centos-7 rhel-8 windows-2019 windows-2019-containerd windows-2022-containerd"
-VHD_CI_TARGETS="ubuntu-2004 ubuntu-2204 windows-2019-containerd windows-2022-containerd"
+VHD_TARGETS="ubuntu-1804 ubuntu-2004 ubuntu-2204 centos-7 mariner-2 rhel-8 windows-2019 windows-2019-containerd windows-2022-containerd"
+VHD_CI_TARGETS="ubuntu-2004 ubuntu-2204 mariner-2 windows-2019-containerd windows-2022-containerd"
 SIG_TARGETS="ubuntu-1804 ubuntu-2004 ubuntu-2204 centos-7 rhel-8 windows-2019 windows-2019-containerd windows-2022-containerd flatcar"
 SIG_CI_TARGETS="ubuntu-2004 ubuntu-2204 windows-2019-containerd windows-2022-containerd flatcar"
 SIG_GEN2_TARGETS="ubuntu-1804 ubuntu-2004 ubuntu-2204 centos-7 flatcar"
